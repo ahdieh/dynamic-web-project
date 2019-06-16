50f1dba367b3d0e34a8ab396c789a2a50efefe15
ahdieh <ahdieh.e@gmail.com>
2019-06-16 10:39:13 -0400

    Created output.jsp to show the response to the client

--- dynamic-web-page/src/com/ahdieh/ServletExample.java
+++ dynamic-web-page/src/com/ahdieh/ServletExample.java
@@ -1,10 +1,7 @@
 package com.ahdieh;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,12 +11,18 @@ public class ServletExample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
 		
+		/*if (request.getParameter("firstname") == null || request.getParameter("lastname") == null){
+			getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}*/
 		String firstName = request.getParameter("firstname");
-		String lastName = request.getParameter("lastname");		
+		String lastName = request.getParameter("lastname");
 		
-		out.println(firstName + " " + lastName);
+		request.setAttribute("firstname", firstName);
+		request.setAttribute("lastname", lastName);
+		
+		getServletContext().getRequestDispatcher("/output.jsp").forward(request, response);
 	}
 
 }
