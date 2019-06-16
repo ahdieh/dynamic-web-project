29b4648c8a4ae144ab248874173ecee2b50434fe
ahdieh <ahdieh.e@gmail.com>
2019-06-16 09:52:04 -0400

    modified servlet to interact with the input that user entered in index.jsp

--- dynamic-web-page/src/com/ahdieh/ServletExample.java
+++ dynamic-web-page/src/com/ahdieh/ServletExample.java
@@ -15,7 +15,11 @@ public class ServletExample extends HttpServlet {
  
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("My J2EE Application");
+		
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");		
+		
+		out.println(firstName + " " + lastName);
 	}
 
 }
